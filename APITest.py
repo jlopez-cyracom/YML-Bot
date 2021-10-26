8dde393f0439cd53b823da61e925e66179bd8f10
Jesus Lopez <jebus.lpz@gmail.com>
2021-10-25 23:05:47 -0600

    10/25/2021 Changes

--- APITest.py
+++ APITest.py
@@ -1,36 +1,44 @@
 import requests
 import pprint
 import json
-import datetime
+from datetime import datetime, timedelta
 
-now = datetime.datetime.now()
-date = str(now)
-print(date[0:10])
+today = datetime.now()
+yesterday = datetime.now() - timedelta(1)
+print("today =", today)
+print("yesterday =", yesterday)
+
+# now = datetime.datetime.now()
+# date = str(now)
+# print(date[0:10])
 
 
+string = "!stock AAPL"
+newString = string.replace("!stock ", "", 1)
+print(newString)
 
-#response = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=EE2617D0CA6B2A2A5EDB583F07C86DD7&format=json")
+# Check to see if there was en error with the call
+if response.json()["status"] == "ERROR":
+    if response.json()["error"] == "today's Date not supported yet":
+        # Make the call again but with yesterday's date
+        yesterday = now - timedelta(2)
+        stringYesterday = str(yesterday)
+        newApiCall = "https://api.polygon.io/v1/open-close/" + stockSearch + "/" + stringYesterday[
+                                                                                   0:10] + "?adjusted=true&apiKey=ox0cWEbdfMMTQTDO2reGQ9TaGpWf7ugp"
+        newResponse = requests.get(newApiCall)
+        # https://api.polygon.io/v1/open-close/AAPL/2021-10-02?adjusted=true&apiKey=ox0cWEbdfMMTQTDO2reGQ9TaGpWf7ugp
+        # https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=ox0cWEbdfMMTQTDO2reGQ9TaGpWf7ugp
+        print(newApiCall)
+        print(newResponse.json())
+        await message.channel.send(
+            "Fucken early bird, you beat the market open. You should be asleep right now. It's people like"
+            " you that keep me up programming shit like this. Here is yesterday's information you cunt")
+        await message.channel.send("Open - " + newResponse.json()["open"] +
+                                   "\nHigh - " + newResponse.json()["high"] +
+                                   "\nLow - " + newResponse.json()["low"] +
+                                   "\nClose - " + newResponse.json()["close"])
 
-# data = response.json()
-# with open("steamLibraryComplete.json", "w", encoding="utf-8") as file:
-#     json.dump(data, file, ensure_ascii=False, indent=4)
-
-#pprint.pprint(response.json())
+    else:
+        await message.channel.send(response.json()["error"])
 
 # Steam API Key EE2617D0CA6B2A2A5EDB583F07C86DD7
-
-
-# print("--------")
-# print("  Open \n--------")
-# print(response.json()["open"])
-# print("")
-#
-# print("--------")
-# print("  Close \n--------")
-# print(response.json()["close"])
-# print("")
-#
-# print("--------")
-# print("  High \n--------")
-# print(response.json()["high"])
-# print("")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import os
-import datetime
+from datetime import datetime, timedelta
 import random
 import discord
 import requests
@@ -64,10 +64,18 @@ async def on_message(message):
 
 # -------------------------------------------- Commands ----------------------------------------------------------------
     if message.content.startswith("!help"):
-        await message.channel.send("Here are all the commands I can do:\n" +
-                                   "\n (!help) - Do I really need to tell you what this does?"
-                                   "\n (!about) - Want to know more about me?"
-                                   "\n (!bullyFemtits) - Send a message to Femtoz that he will absolutely LOVE!")
+        await message.channel.send("AVAILABLE COMMANDS\n"
+                                   "----------------------------------------------"
+                                   "\n **!help** - Do I really need to tell you what this does?"
+                                   "\n **!about** - Want to know more about me?"
+                                   "\n **!bullyFemtits** - Send a message to Femtoz that he will absolutely LOVE"
+                                   "\n **!stock {ticker}** - Shows the Open, High, Low and Close of the specified ticker\n"
+                                   "\nCOMING SOON\n"
+                                   "----------------------------------------------"
+                                   "\n**!crypto {coin}** - Show the Open, High, Low and Close of the specified coin"
+                                   "\n**!newGame** - When you don't know what to play and are willing to buy a new cheap game"
+                                   "\n**!newGameCoop** - In case you don't like to try new games alone"
+                                   "\n**!randomGame** - Becasue we all have huge steam libraries and can never decide what to play, this will choose one for you")
 
     if message.content.startswith("!about"):
         await message.channel.send(about)
@@ -77,16 +85,62 @@ async def on_message(message):
 
     # Detecting a ticker and sending an API call to find the OPEN/CLOSE for that ticker
     if message.content.startswith("!stock "):
-        # Grab the current date to use in API call
-        now = datetime.datetime.now()
-        date = str(now)  # Casting to string
+        # --------------------- Grab the last valid date to use in API call ---------------------
+        yesterday = datetime.now() - timedelta(1)
+        if yesterday.weekday() == 5:
+            yesterday -= timedelta(1)
+        elif yesterday.weekday() == 6:
+            yesterday -= timedelta(2)
+        validDate = str(yesterday)  # Casting to string
 
-        stockSearh = message.content
-        stockSearh.replace("!stock ", "", 1)
-        apiCall = "https://api.polygon.io/v1/open-close/" + stockSearh + "/" + date[0:10] +"?adjusted=true&apiKey=ox0cWEbdfMMTQTDO2reGQ9TaGpWf7ugp"
+        # --------------------- Grabbing the ticker from the command ---------------------
+        ticker = message.content.replace("!stock ", "")
+
+        # --------------------- Make the call ---------------------
+        apiCall = "https://api.polygon.io/v1/open-close/" + ticker.upper() + "/" + validDate[0:10] +"?adjusted=true&apiKey=ox0cWEbdfMMTQTDO2reGQ9TaGpWf7ugp"
         response = requests.get(apiCall)
-        # Now we just have to construct the message that will be sent to discord to show the information
-        # Do it in try/catch format to check if the result from the api call is null
+
+        print(apiCall)
+        print(response.json())
+
+        # --------------------- Error handling for different status responses ---------------------
+        # *******Implement status codes instead of strings
+        status = response.status_code
+        if status == 404:
+            await message.channel.send("The ticker you searched for is not supported/invalid: " + str(response.json()["message"]))
+        elif status == 200:
+            await message.channel.send("-----------------------" +
+                                       "\n             " + ticker.upper() +
+                                       "\n-----------------------" +
+                                       "\n* ᴰᵘᵉ ᵗᵒ ᵗʰᵉ ⁿᵃᵗᵘʳᵉ ᵒᶠ ᵗʰᵉ ˢᵒᵘʳᶜᵉ ᵒᶠ ᵗʰᵉ ⁱⁿᶠᵒʳᵐᵃᵗⁱᵒⁿ, ᵗʰᵉ ⁿᵘᵐᵇᵉʳˢ ʸᵒᵘ ˢᵉᵉ ᵃʳᵉ ᵇᵃˢᵉᵈ ᵒⁿ ᵗʰᵉ ˡᵃˢᵗ ᵛᵃˡⁱᵈ ᵗʳᵃᵈⁱⁿᵍ ᵈᵃᵗᵉ" +
+                                       "\nOpen - " + str(response.json()["open"]) +
+                                       "\nHigh - " + str(response.json()["high"]) +
+                                       "\nLow - " + str(response.json()["low"]) +
+                                       "\nClose - " + str(response.json()["close"])
+                                       )
+        # --------------------- Formulate the message that is going to be sent ---------------------
+        else:
+            await message.channel.send("You should never see this...if you do, you royaly fucked up haha")
+
+    if message.content.startswith("!crypto "):
+        today = str(datetime.now())
+        ticker = message.content.replace("!crypto ", "")
+        apiCall = "https://api.polygon.io/v1/open-close/crypto/" + ticker.upper() + "/USD/" + today[0:10] + "?adjusted=true&apiKey=ox0cWEbdfMMTQTDO2reGQ9TaGpWf7ugp"
+        response = requests.get(apiCall)
+        jsonResponse = response.json()
+
+        print(apiCall)
+        print(jsonResponse)
+
+        await message.channel.send("-----------------------" +
+                                   "\n             " + ticker.upper() +
+                                   "\n-----------------------" +
+                                   "\n* ᴰᵘᵉ ᵗᵒ ᵗʰᵉ ⁿᵃᵗᵘʳᵉ ᵒᶠ ᵗʰᵉ ˢᵒᵘʳᶜᵉ ᵒᶠ ᵗʰᵉ ⁱⁿᶠᵒʳᵐᵃᵗⁱᵒⁿ, ᵗʰᵉ ⁿᵘᵐᵇᵉʳˢ ʸᵒᵘ ˢᵉᵉ ᵃʳᵉ ᵇᵃˢᵉᵈ ᵒⁿ ᵗʰᵉ ˡᵃˢᵗ ᵛᵃˡⁱᵈ ᵗʳᵃᵈⁱⁿᵍ ᵈᵃᵗᵉ" +
+                                   "\nOpen - " + str(jsonResponse["open"]) +
+                                   "\nHigh - " + str(jsonResponse["high"]) +
+                                   "\nLow - " + str(jsonResponse["low"]) +
+                                   "\nClose - " + str(jsonResponse["close"])
+                                   )
 
     # How to send DM's
     if message.content.startswith("!test"):
